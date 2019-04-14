53352e6f368c8a1ff7732725e344a0c1cba98697
Himanshu Pandit <himanshu.pandit@outlook.com>
2019-04-14 18:22:28 -0400

    Updates

---
+++ precache-manifest.6fc859cb90293721ec484d8771473e28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "900a30614569055077d1",
+    "url": "/ClickyGame/static/js/main.f76de7b5.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "900a30614569055077d1",
+    "url": "/ClickyGame/static/css/main.9b7b1a1e.chunk.css"
+  },
+  {
+    "revision": "f8840328b2fbb0517ca3d9c20b4ecc2d",
+    "url": "/ClickyGame/index.html"
+  }
+];
